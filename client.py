70fe76212942972c44bdd13257763647b5a6a075
MissBlumarine <106033398+MissBlumarine@users.noreply.github.com>
2023-01-14 20:07:32 +0300

    Update client.py
    добавлены комментарии

--- client.py
+++ client.py
@@ -1,9 +1,15 @@
 import socket
 
 
-def senter(text):
-    b = bytes(text, 'utf-8')
+def senter(text: str):
+    
+    # переводим введенную строку в байты, т.к. сервер принимает данные в виде байт
+    b = bytes(text, 'utf-8') 
+    
+    # создаем сокет
     sock = socket.socket()
+    
+    # вводим данные хоста и порта сервера
     sock.connect(('', 9090))
     sock.send(b)
 
